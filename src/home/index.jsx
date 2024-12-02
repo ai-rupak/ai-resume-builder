7f2e08bdd9f01fbb98cab766d8d102883f27aa1e
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2024-12-02 09:54:45 +0530

    first commit

---
+++ src/home/index.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/custom'
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        
+        <h1>Welcome to my home page</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SigninPage from './auth/sign-in'
+import Home from './home'
+import Dashboard from './dashboard'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const router = createBrowserRouter([
+  {
+    
+    element: <App />,
+    children: [
+      
+      {
+        path: '/dashboard',
+        element: <Dashboard/>,
+      }
+
+    ]
+  },
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path:'/auth/sign-in',
+    element: <SigninPage/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router}/>
+    </ClerkProvider>
+  </StrictMode>,
+)
