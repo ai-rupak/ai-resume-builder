50d02d5cebb8dab199ae621dfcab49bc39a9db8e
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2025-07-29 16:55:22 +0530

    ✨

--- src/my-resume/[resumeId]/download/index.jsx
+++ src/my-resume/[resumeId]/download/index.jsx
@@ -31,29 +31,31 @@ function ViewResume() {
 
   
     return (
-        <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}} >
-            <div className='min-h-screen bg-gray-50 flex flex-col'>
-                {/* Fixed header */}
-                <div className='fixed top-0 left-0 right-0 z-50'>
-                    <Header/>
-                </div>
+        
+        // <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}} >
+        //     <div className='min-h-screen bg-gray-50 flex flex-col'>
+        //         {/* Fixed header */}
+        //         <div className='fixed top-0 left-0 right-0 z-50'>
+        //             <Header/>
+        //         </div>
                 
-                {/* Main content container with top padding */}
-                <div id="no-print" className="pt-16 "> {/* Adjust pt-16 to match header height */}
-                    <div className="flex grow">
-                        {/* Left Sidebar */}
-                        <Download_Sidebar resumeId={resumeId} resumeInfo={resumeInfo}/>
+        //         {/* Main content container with top padding */}
+        //         <div id="no-print" className="pt-16 "> {/* Adjust pt-16 to match header height */}
+        //             <div className="flex grow">
+        //                 {/* Left Sidebar */}
+        //                 {/* <Download_Sidebar resumeId={resumeId} resumeInfo={resumeInfo}/> */}
                         
-                        {/* Main Content */}
-                        <div className="flex-1 flex justify-center items-center">
-                            <div className="p-3 w-full md:w-1/2 max-w-2xl overflow-y-auto ">
-                                {/* <Download_ResumePreview /> */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </ResumeInfoContext.Provider>
+        //                 {/* Main Content */}
+        //                 <div className="flex-1 flex justify-center items-center">
+        //                     <div className="p-3 w-full md:w-1/2 max-w-2xl overflow-y-auto ">
+        //                         {/* <Download_ResumePreview /> */}
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </ResumeInfoContext.Provider>
+        <div><Header /></div>
     )
 
 }
